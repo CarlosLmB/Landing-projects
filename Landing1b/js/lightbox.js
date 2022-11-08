23465e6d948a478fe2e4f215fce04ec7dcf4335c
Carlos Cordero <108688592+CarlosLmB@users.noreply.github.com>
2022-11-07 23:45:45 -0400

    Add files via upload

---
+++ Landing1b/js/lightbox.js
@@ -0,0 +1,28 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+const hamburguesa1 = document.querySelector(".hamburguesa");
+
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+
+    })
+})
+
+contenedorLight.addEventListener('click', (e)=>{
+    if(e.target !== imagenesLight){
+        contenedorLight.classList.toggle('show')
+        imagenesLight.classList.toggle('showImage')
+        hamburguesa1.style.opacity = '1'
+    }
+} )
+
+const aparecerImagen = (imagen)=>{
+    imagenesLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenesLight.classList.toggle('showImage')
+    hamburguesa1.style.opacity = '0'
+
+}
---
+++ Landing1b/js/menu.js
@@ -0,0 +1,18 @@
+const hamburguesa = document.querySelector(".hamburguesa");
+const menu = document.querySelector(".menu");
+
+
+//console.log(menu)
+//console.log(hamburguesa)
+
+
+hamburguesa.addEventListener("click", ()=>{ 
+menu.classList.toggle("spread")
+})
+
+window.addEventListener("click", e=>{
+    if(menu.classList.contains("spread")
+        && e.target != menu && e.target != hamburguesa ){
+            menu.classList.toggle("spread")
+        }
+})
